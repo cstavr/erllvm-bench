770a57f0313d0e551460d0bde846cfceaff41e20
Yiannis Tsiouris <gtsiour@softlab.ntua.gr>
2012-03-20 14:36:03 +0200

    Issue #4: Run only selected benchmark class ;-)

--- run.sh
+++ run.sh
@@ -1,33 +1,16 @@
 #!/bin/sh
 
 ## Executes all benchmarks
-main ()
+run_all ()
 {
     OTP_ROOT=$1
     ITERS=$2
     echo "Running all benchmark classes..."
 
-    ## If runtime/results.res exists rename it:
-    if [ -e results/runtime.res ]; then
-        NEW_RES=runtime.res-`date +"%y.%m.%d-%H:%M:%S"`
-        mv results/runtime.res results/$NEW_RES
-    fi
-
-    ## Get failing
-    if [ -r failing ]; then
-        skipped=`cat failing`
-    else
-        skipped=
-    fi
-
-    for i in `seq 1 $ITERS`; do
-        echo "Iter $i/$ITERS:"
-        ## Look for all available Classes to run
-        for c in `find ebin/ -maxdepth 1 -mindepth 1 -type d`; do
-            CLASS=`basename $c`
-            run_class $OTP_ROOT $CLASS
-        done
-        awk '{btl += $9 ;htl +=$11} END {print "Runtime BTL:", btl/NR, "Runtime HTL:", htl/NR, "lines", NR}' results/runtime.res
+    ## Look for all available Classes to run
+    for c in `find ebin/ -maxdepth 1 -mindepth 1 -type d`; do
+        CLASS=`basename $c`
+        run_class $OTP_ROOT $CLASS
     done
 }
 
@@ -36,6 +19,13 @@ run_class ()
     OTP_ROOT=$1
     CLASS=$2
     echo "   [Class] $CLASS"
+
+    ## Get failing
+    if [ -r failing ]; then
+        skipped=`cat failing`
+    else
+        skipped=
+    fi
 
     for f in `ls ebin/$CLASS/*.beam`; do
         BENCH=`basename $f .beam`
@@ -65,9 +55,36 @@ run_benchmark ()
     $OTP_ROOT/bin/erl -pa ebin/ $EBIN_DIRS -noshell -s run_benchmark run $BENCH -s erlang halt
 }
 
-if [ $# -ne 2 ]; then
-    echo "Usage: `basename $0` OTP_DIR ITERATIONS"
-    exit 1
-fi
+main ()
+{
+    if [ $# -eq 2 ]; then
+        RUN=run_all
+        OTP_ROOT=$1
+        ITERS=$2
+    else
+        if [ $# -eq 3 ]; then
+            RUN=run_class
+            OTP_ROOT=$1
+            ITERS=$2
+            CLASS=$3
+        else
+            echo "Usage: `basename $0` OTP_DIR ITERATIONS <CLASS>"
+            exit 1
+        fi
+    fi
 
-main $1 $2
+    ## Run $ITERS times:
+    for i in `seq 1 $ITERS`; do
+        ## If runtime/results.res exists rename it:
+        if [ -e results/runtime.res ]; then
+            NEW_RES=runtime.res-`date +"%y.%m.%d-%H:%M:%S"`
+            mv results/runtime.res results/$NEW_RES
+        fi
+
+        echo "Iter $i/$ITERS:"
+        $RUN $OTP_ROOT $CLASS
+        awk '{btl += $9 ;htl +=$11} END {print "Runtime BTL:", btl/NR, "Runtime HTL:", htl/NR}' results/runtime.res
+    done
+}
+
+main $1 $2 $3
