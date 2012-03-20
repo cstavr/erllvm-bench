cc0fcfd0636f66eaac3682cc3d038ddbf204ca75
Yiannis Tsiouris <gtsiour@softlab.ntua.gr>
2012-03-20 11:06:29 +0200

    Abstracted run_class() and fixed iterations bug

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-# Executes all benchmarks
+## Executes all benchmarks
 main ()
 {
     echo "Running all benchmark classes..."
 
-    # If runtime/results.res exists rename it:
+    ## If runtime/results.res exists rename it:
     if [ -e results/runtime.res ]; then
         NEW_RES=runtime.res-`date +"%y.%m.%d-%H:%M:%S"`
         mv results/runtime.res results/$NEW_RES
     fi
 
-    # Get failing
+    ## Get failing
     if [ -r failing ]; then
         skipped=`cat failing`
     else
@@ -20,28 +20,34 @@ main ()
 
     for i in `seq 1 $2`; do
         echo "Iter $i:"
-        # Look for BEAM files to run
+        ## Look for all available Classes to run
         for c in `find ebin/ -maxdepth 1 -mindepth 1 -type d`; do
             class=`basename $c`
-            echo "   [Class] $class"
-            for f in `ls $c/*.beam`; do
-                bench=`basename $f .beam`
-                # Skip file if in failing
-                skip="no"
-                for s in $skipped; do
-                    if [ "$bench" = "$s" ]; then
-                        skip="yes"
-                        break
-                    fi
-                done
-                if [ "$skip" = "yes" ]; then
-                    continue
-                fi
-                # Else run benchmark
-                run_benchmark $bench $class
-            done
+            run_class $class
         done
         awk '{btl += $9 ;htl +=$11} END {print "Runtime BTL:", btl/NR, "Runtime HTL:", htl/NR, "lines", NR}' results/runtime.res
+    done
+}
+
+run_class ()
+{
+    class=$1
+    echo "   [Class] $class"
+    for f in `ls ebin/$class/*.beam`; do
+        bench=`basename $f .beam`
+        ## Skip file if in failing
+        skip="no"
+        for s in $skipped; do
+            if [ "$bench" = "$s" ]; then
+                skip="yes"
+                break
+            fi
+        done
+        if [ "$skip" = "yes" ]; then
+            continue
+        fi
+        ## Else run benchmark
+        run_benchmark $bench $class
     done
 }
 
@@ -57,4 +63,4 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-main
+main $1 $2
