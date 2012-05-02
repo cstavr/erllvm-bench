5830eecd2b4bef3ffa3f4a22b2151bed97707bbe
Yiannis Tsiouris <yiannis.tsiouris@gmail.com>
2012-05-02 21:04:24 +0300

    Use 'getopt' for argument parsing in driver

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ run_all ()
 
     ## Look for all available Classes to run
     for c in `find ebin/ -maxdepth 1 -mindepth 1 -type d`; do
-        CLASS=`basename $c`
-        run_class $OTP_ROOT $CLASS
+	CLASS=`basename $c`
+	run_class $OTP_ROOT $CLASS
     done
 }
 
@@ -21,26 +21,26 @@ run_class ()
 
     ## Get failing
     if [ -r failing ]; then
-        skipped=`cat failing`
+	skipped=`cat failing`
     else
-        skipped=
+	skipped=
     fi
 
     for f in `ls ebin/$CLASS/*.beam`; do
-        BENCH=`basename $f .beam`
-        ## Skip file if in failing
-        SKIP="no"
-        for s in $skipped; do
-            if [ "$BENCH" = "$s" ]; then
-                SKIP="yes"
-                break
-            fi
-        done
-        if [ "$SKIP" = "yes" ]; then
-            continue
-        fi
-        ## Else run benchmark
-        run_benchmark $OTP_ROOT $BENCH
+	BENCH=`basename $f .beam`
+	## Skip file if in failing
+	SKIP="no"
+	for s in $skipped; do
+	    if [ "$BENCH" = "$s" ]; then
+		SKIP="yes"
+		break
+	    fi
+	done
+	if [ "$SKIP" = "yes" ]; then
+	    continue
+	fi
+	## Else run benchmark
+	run_benchmark $OTP_ROOT $BENCH
     done
 }
 
@@ -73,37 +73,86 @@ plot_diagram ()
     rm -rf $TMP_DIR
 }
 
+usage ()
+{
+    cat << EOF
+Usage: $0 options OTP_ROOT
+
+This script runs the benchmarks using the provided OTP directory (first
+non-option argument) and creates the corresponding diagrams.
+
+OPTIONS:
+  -h    Show this message
+  -a    Run all available benchmarks
+  -c    Benchmark class to run
+  -n    Number of iterations
+EOF
+}
+
 main ()
 {
-    if [ $# -eq 2 ]; then
-        RUN=run_all
-        OTP_ROOT=$1
-        ITERS=$2
-    else
-        if [ $# -eq 3 ]; then
-            RUN=run_class
-            OTP_ROOT=$1
-            ITERS=$2
-            CLASS=$3
-        else
-            echo "Usage: `basename $0` OTP_DIR ITERATIONS <CLASS>"
-            exit 1
-        fi
+    RUN=
+    BENCH_CLASS=
+    ITERATIONS=1
+    DEBUG=0
+
+    while getopts "hadn:c:" OPTION; do
+	case $OPTION in
+	    h|\?)
+		usage
+		exit 0
+		;;
+	    a)
+		RUN=run_all
+		;;
+	    c) ## Run *only* specified benchmark class:
+		RUN=run_class
+		BENCH_CLASS=$OPTARG
+		;;
+	    n)
+		ITERATIONS=$OPTARG
+		;;
+	    d)
+		DEBUG=1
+		;;
+	esac
+    done
+
+    ## $1 is now the first non-option argument, $2 the second, etc
+    shift $(($OPTIND - 1))
+    OTP_ROOT=$1
+
+    ## If RUN is not set something went wrong (probably the script was called
+    ## with no args):
+    if [ -z $RUN ]; then
+	usage
+	exit 1
     fi
 
-    ## Run $ITERS times:
-    for i in `seq 1 $ITERS`; do
-        echo "Iter $i/$ITERS:"
-        echo "### Benchmark BEAM/ErLLVM HiPE/ErLLVM BEAM HiPE ErLLVM" > results/runtime.res
-        $RUN $OTP_ROOT $CLASS
-        awk '{btl += $3 ;htl += $5} END {print "Runtime BTL:", btl/NR, "Runtime HTL:", htl/NR}' results/runtime.res
+    if [ $DEBUG -eq 1 ]; then
+	cat << EOF
+-- Debug info:
+  Iter  = $ITERATIONS
+  Run   = $RUN
+  Class = $BENCH_CLASS
+  OTP   = $OTP_ROOT
+EOF
+    fi
+
+    ## Run $ITERATIONS times:
+    for i in `seq 1 $ITERATIONS`; do
+	echo "Iter $i/$ITERATIONS:"
+	echo "### Benchmark BEAM/ErLLVM HiPE/ErLLVM BEAM HiPE ErLLVM" > results/runtime.res
+	$RUN $OTP_ROOT $BENCH_CLASS
+	awk '{btl += $3 ;htl += $5} END {print "Runtime BTL:", btl/NR, "Runtime HTL:", htl/NR}' \
+	    results/runtime.res
 
         ## Copy results to another .res file:
-        NEW_RES=runtime-`date +"%y.%m.%d-%H:%M:%S"`.res
-        mv results/runtime.res results/$NEW_RES
+	NEW_RES=runtime-`date +"%y.%m.%d-%H:%M:%S"`.res
+	mv results/runtime.res results/$NEW_RES
 
-        plot_diagram $NEW_RES
+	plot_diagram $NEW_RES
     done
 }
 
-main $1 $2 $3
+main $@
