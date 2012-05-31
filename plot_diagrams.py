ac560077c41d9b5b27749aeccb52d2a9721fbf2d
Christos Stavrakakis <cstavr@grnet.gr>
2012-05-31 12:10:42 +0300

    Fix plotting script
    Still ugly!

--- plot_diagrams.py
+++ plot_diagrams.py
@@ -1,7 +1,9 @@
 import os
 import glob
+
 SYS = ['beam', 'hipe', 'erllvm', 'erjang', 'erjang_1']
 
+## MISC FUNCTIONS
 def pretty_name(name):
     if name == 'beam':
         return 'Beam'
@@ -13,6 +15,34 @@ def pretty_name(name):
         return 'Erjang-1'
     if name == 'erjang':
         return 'Erjang'
+
+def print_line(f, line):
+    line = map(lambda x: str(x).ljust(15), line)
+    line = ''.join(line)
+    f.write(line + '\n')
+
+def print_plot_info(f, cluster, mmax):
+    f.write("# clustered graph example from Derek Bruening's CGO 2005 talk\n")
+    cluster = "=cluster;%s\n" % cluster
+    f.write(cluster + '\n')
+    f.write("# green instead of gray since not planning on printing this\n")
+    f.write("colors=grey5,med_blue,light_green,red,yellow\n")
+    f.write("=arithmean\n")
+    f.write("meanlabel=Average\n")
+    f.write("=table\n")
+    f.write("yformat=%g\n")
+    f.write("max=%s\n" % str(mmax))
+    f.write("ylabel=Runtime Speedup\n")
+    f.write("xlabel=Benchmark\n")
+    f.write("rotateby=-40\n")
+    f.write("horizline=1\n")
+    f.write("# stretch it out in x direction\n\n")
+
+
+### MAIN
+
+
+# Get results from files
 results = {}
 for name in SYS:
     results_ = {}
@@ -27,87 +57,62 @@ for name in SYS:
     results[name] = results_
     f.close()
 
+# Assert all benchmarks run
 for name in SYS[1:]:
     assert len(results[name]) == len(results[SYS[0]])
 
 benchmarks = results['beam'].keys()
 platforms = SYS
 
+# Revert table
 bench_results = {}
+bench_results['shootout'] = {}
+bench_results['misc'] = {}
 for bench_name in benchmarks:
-    bench_results[bench_name] = {}
+    fname1 = 'src/shootout/' + bench_name + '.erl'
+    if os.path.exists(fname1):
+        category = 'shootout'
+    else:
+        category = 'misc'
+    bench_results[category][bench_name] = {}
     for name in platforms:
-        bench_results[bench_name][name] = results[name][bench_name]
+        bench_results[category][bench_name][name] = results[name][bench_name]
 
+# Print merged results table
 f = open('results/runtime_total.res', 'w')
-line = 'Name'.ljust(15)
-for plat in platforms:
-    line += plat.ljust(15)
-f.write('#'+line+'\n')
-
-for name in benchmarks:
-    line = name.ljust(15)
-    for plat in platforms:
-        line += (bench_results[name][plat]).ljust(15)
-    f.write(line+'\n')
+print_line(f, ['#Name']+platforms)
+for cat in ['shootout', 'misc']:
+    for name in sorted(bench_results[cat].keys()):
+        line = []
+        for plat in platforms:
+            value = bench_results[cat][name][plat]
+            bench_results[cat][name][plat] = float(value)
+            line.append(value)
+        print_line(f, line)
 f.close()
 
-mmax = 0
-for plat in platforms:
-    for bench in benchmarks:
-        value = float(bench_results[bench][plat])
-        bench_results[bench][plat] = value
-        if value > mmax:
-            mmax = value
 
-foo = "# clustered graph example from Derek Bruening's CGO 2005 talk\n"
-cluster = "=cluster;Beam;HiPE;ErLLVM;Erjang-1;Erjang-6i\n"
-foo1 = \
-"# green instead of gray since not planning on printing this\n"\
-"colors=grey5,med_blue,light_green,red,yellow\n"\
-"=arithmean\n"\
-"meanlabel=Average\n"\
-"=table\n"\
-"yformat=%g\n"
-mmax = "max= %s" % str(mmax)
-foo3 =\
-"""
-ylabel=Runtime Speedup
-xlabel=Benchmark
-rotateby=-40
-horizline=1
-# stretch it out in x direction
-# extraops=set label "5.66" at 14,4.2 right font "Times,10"
-
-
-
-"""
 
 for cat in ['shootout', 'misc']:
     for drop_plat in platforms:
         rest_plat = list(platforms)
         rest_plat.remove(drop_plat)
+
         f_from = open('results/speed_from_'+drop_plat+'_'+cat+'.perf', 'w')
         f_to = open('results/speed_to_'+drop_plat+'_'+cat+'.perf', 'w')
 
+
+        # Find max (!ugly)
         from_max = 0
         to_max = 0
-        for name in benchmarks:
-            if cat == 'shootout':
-                fname1 = 'src/shootout/'+name+'.erl'
-                fname2 = ''
-            else:
-                fname1 = 'src/small/'+name+'.erl'
-                fname2 = 'src/orbit-int/'+name+'.erl'
-            if os.path.exists(fname1) or os.path.exists(fname2):
-                for plat in rest_plat:
-                    from_val =  round(bench_results[name][drop_plat] / bench_results[name][plat], 3)
-                    to_val =  round(bench_results[name][plat] / bench_results[name][drop_plat], 3)
-
-                    if to_val > to_max:
-                        to_max = to_val
-                    if from_val > from_max:
-                        from_max = from_val
+        for name in sorted(bench_results[cat].keys()):
+            for plat in rest_plat:
+                from_val =  round(bench_results[cat][name][drop_plat] / bench_results[cat][name][plat], 3)
+                to_val =  round(bench_results[cat][name][plat] / bench_results[cat][name][drop_plat], 3)
+                if to_val > to_max:
+                    to_max = to_val
+                if from_val > from_max:
+                    from_max = from_val
 
         from_cluster=[]
         to_cluster=[]
@@ -115,41 +120,37 @@ for cat in ['shootout', 'misc']:
             from_cluster.append(pretty_name(drop_plat)+'/'+pretty_name(plat))
             to_cluster.append(pretty_name(plat)+'/'+pretty_name(drop_plat))
 
-        from_cluster = '=cluster;'+';'.join(from_cluster)+'\n'
-        to_cluster = '=cluster;'+';'.join(to_cluster)+'\n'
-        from_max = 'max=%s' % str(from_max+1)
-        to_max = 'max=%s' % str(to_max+1)
+        # Print ploting info
+        from_cluster = ';'.join(from_cluster)+'\n'
+        to_cluster = ';'.join(to_cluster)+'\n'
+        from_max = str(from_max + 1)
+        to_max = str(to_max + 1)
+        print_plot_info(f_from, from_cluster, from_max)
+        print_plot_info(f_to, to_cluster, to_max)
 
-        f_from.write(foo+from_cluster+foo1+from_max+foo3)
-        f_to.write(foo+to_cluster+foo1+to_max+foo3)
 
-        from_line = '# Name'.ljust(15)
-        to_line = '# Name'.ljust(15)
+
+        from_line = []
+        to_line = []
         for plat in rest_plat:
-            from_line += (drop_plat+'/'+plat).ljust(15)
-            to_line += (plat+'/'+drop_plat).ljust(15)
-        f_from.write(from_line+'\n')
-        f_to.write(to_line+'\n')
-
-        for name in benchmarks:
-            if cat == 'shootout':
-                fname1 = 'src/shootout/'+name+'.erl'
-                fname2 = ''
-            else:
-                fname1 = 'src/small/'+name+'.erl'
-                fname2 = 'src/orbit-int/'+name+'.erl'
-            if os.path.exists(fname1) or os.path.exists(fname2):
-                from_line = name.ljust(15)
-                to_line = name.ljust(15)
-                for plat in rest_plat:
-                    from_val =  round(bench_results[name][drop_plat] / bench_results[name][plat], 3)
-                    to_val =  round(bench_results[name][plat] / bench_results[name][drop_plat], 3)
-                    from_line += str(from_val).ljust(15)
-                    to_line += str(to_val).ljust(15)
+            from_line.append(drop_plat + '/' + plat)
+            to_line.append(plat + '/' + drop_plat)
+        print_line(f_from, ['#Name'] + from_line)
+        print_line(f_to, ['#Name'] + to_line)
 
 
-                f_from.write(from_line+'\n')
-                f_to.write(to_line+'\n')
+        for name in sorted(bench_results[cat].keys()):
+            from_line = [name]
+            to_line = [name]
+            for plat in rest_plat:
+                from_val =  round(bench_results[cat][name][drop_plat] / bench_results[cat][name][plat], 3)
+                to_val =  round(bench_results[cat][name][plat] / bench_results[cat][name][drop_plat], 3)
+                from_line.append(from_val)
+                to_line.append(to_val)
+
+
+            print_line(f_from, from_line)
+            print_line(f_to, to_line)
 
 
         f_from.close()
